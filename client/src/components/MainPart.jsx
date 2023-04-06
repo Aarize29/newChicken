1f03a865791d2d62ac99f47ebe1371a72920cd5a
Aarize29 <1ds21ai045@dsce.edu.in>
2023-04-07 00:12:57 +0530

    Update MainPart.jsx

--- client/src/components/MainPart.jsx
+++ client/src/components/MainPart.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './MainPart.css'
 const MainPart = () => {
   return (
-    <div>
-      MainPart
+    <div className='flex mt-5'>
+       <div className="model columns-12 border-[Red] h-[600px] border-solid border-[2px] m-3 ">
+            I am Model
+       </div>
+       <div className="chatbot columns-5 border-[Red] h-[600px] border-solid border-[2px] m-3">
+            I am Chatbot
+       </div>
     </div>
   )
 }
