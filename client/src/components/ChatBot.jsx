c827d145f4b7ddac28ebc5cb50a5568c2e8ad3c4
Aarize29 <1ds21ai045@dsce.edu.in>
2023-04-20 23:02:00 +0530

    file uplaod function

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import { BsFillSendFill } from "react-icons/bs";
 const ChatBot = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
   return (
     <div>
       <div className="chatbot flex flex-col justify-between  w-[500px] border-[Red] h-[600px] border-solid border-[2px] m-3 bg-[white]">
@@ -8,8 +13,9 @@ const ChatBot = () => {
                 <h1 className="text-2xl font-bold ">Consultant Bot</h1>
             </div>
 
-            <form >
+            <form onSubmit={handleSubmit} >
                <input type="text" className="m-2 p-2 w-[480px] h-[50px] border-[2px] border-solid border-[black] rounded-[5px]" placeholder='Ask more questions related to disease....'/>
+               <button className='text-xl absolute right-[30px] bottom-[50px]' onClick={handleSubmit}><BsFillSendFill/></button>
             </form>
        </div>
     </div>
--- client/src/components/MainPart.jsx
+++ client/src/components/MainPart.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './MainPart.css'
 import ChatBot from './ChatBot'
 const MainPart = () => {
+  const [imageUpload, setImageUpload] = useState(null)
+  const [form,setForm]=useState({
+      name:'',
+      prompt:'',
+      photo:'',
+  })
+  const handleUpload = (e) => {
+    const file=e.target.files[0]
+    setImageUpload(file);
+    if(file && file.type.substr(0,5)==='image'){
+        const reader=new FileReader()
+        reader.onloadend=()=>{
+            setForm({...form,photo:reader.result})
+        }
+        reader.readAsDataURL(file)
+    }
+    else{
+        alert('Please upload animage')
+    }
+    }
+   
+    const handleChange=(e)=>{
+      setForm({...form,[e.target.name]:e.target.value})
+  }
+  const handlPrediction=(e)=>{
+    e.preventDefault()
+    alert("Prediction Started")
+  }
+  
   return (
-    <div className='flex justify-between mt-5'>
-       <div className="model flex justify-center items-center w-[950px] border-[Red] h-[600px] border-solid border-[2px] m-3 ">
-            I am Model
+    <div className='flex  justify-between mt-5'>
+       <div className="model flex flex-col  justify-center items-center w-[950px] border-[Red] h-[600px] border-solid border-[2px] m-3 ">
+         <h1 className='text-2xl mb-10  font-bold'>Upload image to detect disease</h1>
+         <form className='flex flex-col items-center justify center' onSubmit={handlPrediction}>
+          <div className='text-xl flex justify-center items-center  border-[black] w-[500px] h-[300px] border-solid border-2 rounded-[15px]'>
+           {form.photo ? <img src={form.photo} alt="uploaded" className='border-[black] w-[500px] h-[300px] border-solid border-2 rounded-[15px]'/> : <h1 className='text-2xl font-bold'>No Image Uploaded</h1>}
+          </div>
+          <div className='flex'>
+                <label htmlFor="photo" className="cursor-pointer">
+                    <div className='mt-3 text-white bg-[#6469ff] hover:text-[#6469ff] hover:bg-blue-200 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>Upload</div>
+                    <input type="file" accept="image/*" name="photo" id="photo" className="sr-only" onChange={handleUpload} />
+                </label>
+                <button className='m-3 text-white bg-[#6469ff] hover:text-[#6469ff] hover:bg-blue-200 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'  type='submit'>Predict</button>
+            </div>
+            </form>
+           <h1 className='text-2xl font-bold  mt-5'>Disease name here</h1>
        </div>
        <ChatBot/>
     </div>
