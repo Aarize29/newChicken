1e6d7ead5dd807fb6603261da8ccb05f215399a9
Aarize29 <1ds21ai045@dsce.edu.in>
2023-04-21 03:39:34 +0530

    chatbot  function

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -1,23 +1,101 @@
-import React from 'react'
-import { BsFillSendFill } from "react-icons/bs";
-const ChatBot = () => {
+import { useState } from 'react'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('hello');
+
+const API_KEY="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"
+
+
+function ChatBot() {
+
+  const [typing,setTyping]=useState(false)
+
+  const [messages, setMessages] = useState([
+    {
+      message:"Hello, I am Chicken 69, I will help you to diagnose your chicken disease. ",
+      sender:"ChatGPT"
+    }
+  ])
+
+  const handleSend= async (message)=>{
+    const newMessage={
+      message:message,
+      sender:"user",
+      direction:"outgoing"
+    }
+    const newMessages=[...messages,newMessage]
+
+    setMessages(newMessages)
+
+    setTyping(true)
+
+    await processMaessageToChatGPT(newMessages)
   }
-  return (
-    <div>
-      <div className="chatbot flex flex-col justify-between  w-[500px] border-[Red] h-[600px] border-solid border-[2px] m-3 bg-[white]">
-            <div className="header flex justify-center items-center h-[50px] w-[496px] bg-[yellow]">
-                <h1 className="text-2xl font-bold ">Consultant Bot</h1>
-            </div>
 
-            <form onSubmit={handleSubmit} >
-               <input type="text" className="m-2 p-2 w-[480px] h-[50px] border-[2px] border-solid border-[black] rounded-[5px]" placeholder='Ask more questions related to disease....'/>
-               <button className='text-xl absolute right-[30px] bottom-[50px]' onClick={handleSubmit}><BsFillSendFill/></button>
-            </form>
-       </div>
+  async function processMaessageToChatGPT(chatMessages){
+
+    let apiMessages=chatMessages.map((messageObject)=>{
+      let role=""
+      if(messageObject.sender==="ChatGPT"){
+        role="assistant"
+      }else{
+        role="user"
+      }
+      return {role:role,content:messageObject.message}
+    })
+
+    const systemMessage={
+      role:"system",
+      content:"speak like a pirate"
+    }
+
+    const apiRequestBody={
+      "model":"gpt-3.5-turbo",
+      "messages" : [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method:"POST",
+      headers:{
+        "Authorization":"Bearer " + API_KEY,
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data)=>{
+      return data.json()
+    }).then((data)=>{
+      console.log(data);
+      console.log(data.choices[0].message.content)
+      setMessages(
+        [...chatMessages,{
+          message:data.choices[0].message.content,
+          sender:"ChatGPT"
+        }]
+      )
+      setTyping(false)
+    })
+  }
+
+  return (
+    <div className="App">
+      <div className="chatbot flex flex-col justify-between  w-[500px] border-[Red] h-[600px] border-solid border-[2px] m-3 bg-[white]">
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior='smooth'
+              typingIndicator={typing?<TypingIndicator content="Wait"/>:null}
+            >
+              {messages.map((message,i)=>{
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='What happened to your Chick!' onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   )
 }
--- client/src/components/MainPart.jsx
+++ client/src/components/MainPart.jsx
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import './MainPart.css'
 import ChatBot from './ChatBot'
+import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-backend-cpu";
+import * as tf from '@tensorflow/tfjs';
+
 const MainPart = () => {
   const [imageUpload, setImageUpload] = useState(null)
   const [form,setForm]=useState({
@@ -8,6 +12,7 @@ const MainPart = () => {
       prompt:'',
       photo:'',
   })
+  console.log(imageUpload)
   const handleUpload = (e) => {
     const file=e.target.files[0]
     setImageUpload(file);
@@ -37,7 +42,7 @@ const MainPart = () => {
          <h1 className='text-2xl mb-10  font-bold'>Upload image to detect disease</h1>
          <form className='flex flex-col items-center justify center' onSubmit={handlPrediction}>
           <div className='text-xl flex justify-center items-center  border-[black] w-[500px] h-[300px] border-solid border-2 rounded-[15px]'>
-           {form.photo ? <img src={form.photo} alt="uploaded" className='border-[black] w-[500px] h-[300px] border-solid border-2 rounded-[15px]'/> : <h1 className='text-2xl font-bold'>No Image Uploaded</h1>}
+           {form.photo ? <img src={form.photo} alt="uploaded" className='border-[black] w-[500px] h-[299px] border-solid border-2 w-[500px] h-[300px] border-solid border-2 rounded-[15px]'/> : <h1 className='text-2xl font-bold'>No Image Uploaded</h1>}
           </div>
           <div className='flex'>
                 <label htmlFor="photo" className="cursor-pointer">
